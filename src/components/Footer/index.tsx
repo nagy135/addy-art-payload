4a27ab9b4ed5493ec6302d5f9d1be8bac0adb292
Viktor Nagy <viktor.nagy1995@gmail.com>
2026-03-15 09:51:38 +0100

    fix: footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -46,15 +46,10 @@ export async function Footer() {
       <div className="border-t border-neutral-200 py-6 text-sm dark:border-neutral-700">
         <div className="container mx-auto flex w-full flex-col items-center gap-1 md:flex-row md:gap-0">
           <p>
-            &copy; {copyrightDate} {copyrightName}
-            {copyrightName.length && !copyrightName.endsWith('.') ? '.' : ''} All rights reserved.
-          </p>
-          <hr className="mx-4 hidden h-4 w-px border-l border-neutral-400 md:inline-block" />
-          <p>Designed in Michigan</p>
-          <p className="md:ml-auto">
-            <a className="text-black dark:text-white" href="https://payloadcms.com">
-              Crafted by Payload
-            </a>
+            Designed by{' '}
+            <Link href="https://github.com/nagy135" rel="noreferrer" target="_blank">
+              github.com/nagy135
+            </Link>
           </p>
         </div>
       </div>
